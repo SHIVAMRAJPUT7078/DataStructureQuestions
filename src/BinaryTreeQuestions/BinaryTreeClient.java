c283924292e52f4cbac131ca52ba8b295331b415
shivam rajput <shivam.rajput_cs18@gmail.com>
2021-08-01 16:58:48 +0530

    has Path Sum problem added from leetcode

--- src/BinaryTreeQuestions/BinaryTreeClient.java
+++ src/BinaryTreeQuestions/BinaryTreeClient.java
@@ -3,13 +3,15 @@ package BinaryTreeQuestions;
 public class BinaryTreeClient {
     public static void main(String[] args) {
         BinaryTree bt = new BinaryTree();
-        bt.displayTree();
+//        bt.displayTree();
 //        bt.height();
-        System.out.println("Height of Tree is : " + bt.height());
-        bt.preOrderTraverse();
-        System.out.println();
-        bt.postOrderTraverse();
-        System.out.println();
-        bt.InOrderTraverse();
+//        System.out.println("Height of Tree is : " + bt.height());
+//        bt.preOrderTraverse();
+//        System.out.println();
+//        bt.postOrderTraverse();
+//        System.out.println();
+//        bt.InOrderTraverse();
+
+
     }
 }
