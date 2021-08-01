e78aaa43564be8927939247df31ef6af65278ae8
shivam rajput <shivam.rajput_cs18@gmail.com>
2021-08-01 15:20:37 +0530

    BinaryTreeClient added

--- src/BinaryTreeQuestions/BinaryTree.java
+++ src/BinaryTreeQuestions/BinaryTree.java
@@ -1,7 +1,85 @@
 package BinaryTreeQuestions;
 
-public class BinaryTree {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class BinaryTree {
+    Scanner sc = new Scanner(System.in);
+
+    private class Node {
+        int value;
+        Node left;
+        Node right;
+
+//        Node(int value){
+//            this.value = value;
+//            this.left = null;
+//            this.right = null;
+//        }
     }
+
+    public BinaryTree() {
+        this.root = this.construct(null, true);
+    }
+
+    private Node root;
+
+    private Node construct(Node parent, boolean ilc) {
+        if (parent == null) { // means this is the root node as root node does not have its parent node
+            System.out.println("Enter the value of root");
+        } else {
+            if (ilc) {
+                System.out.println("Enter the left child of " + parent.value);
+            } else {
+                System.out.println("Enter the right child of " + parent.value);
+            }
+
+        }
+        int data = sc.nextInt();
+        Node node = new Node();
+        node.value = data;
+        System.out.println("Kya " + data + "Ka left child hai");
+        boolean kyaLeftChildHai = sc.nextBoolean();
+        if (kyaLeftChildHai) {
+            node.left = construct(node, true);
+        }
+        System.out.println("Kya " + data + "Ka right child hai");
+        boolean kyaRightChildHai = sc.nextBoolean();
+        if (kyaRightChildHai) {
+            node.right = construct(node, true);
+        }
+        return node;
+    }
+
+    public void displayTree() {
+        this.displayTree(this.root);
+    }
+
+    private void displayTree(Node node) {
+        if (node == null) {
+//            System.out.println(root.value);
+            return;
+        }
+        String str = "";
+        if (node.left == null) {
+            str += '_';
+        } else {
+            str += node.left.value;
+        }
+
+        str += " -> " + node.value + " <- ";
+
+        if (node.right == null) {
+            str += '_';
+        } else {
+            str += node.right.value;
+        }
+
+        System.out.println(str);
+        displayTree(node.left);
+        displayTree(node.right);
+    }
+
+
+// 1 true 2 true 4 false false false true 3 false false
+
 }
